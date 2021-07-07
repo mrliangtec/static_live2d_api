d4ae5d9c0be7374c875ec7a986d887b9aede921b
mrliangtec <XLXL103@outlook.com>
2021-07-07 20:24:22 +0800

    Update autoload.js

--- autoload.js
+++ autoload.js
@@ -9,4 +9,4 @@
     document.write('<div class="waifu"><div class="waifu-tips"></div><canvas id="live2d" class="live2d"></canvas><div class="waifu-tool"><span class="fui-home"></span><span class="fui-chat"></span><span class="fui-eye"></span><span class="fui-user"></span><span class="fui-photo"></span><span class="fui-info-circle"></span><span class="fui-cross"></span></div></div>');
 }())
 setTimeout("/* 可直接修改部分参数 */live2d_settings['modelId'] = 0; // 默认模型 IDlive2d_settings['modelTexturesId'] = 87; // 默认材质 IDlive2d_settings['modelStorage'] = false; // 不储存模型 IDlive2d_settings['canCloseLive2d'] = false; // 隐藏 关闭看板娘 按钮live2d_settings['canTurnToHomePage'] = false; // 隐藏 返回首页 按钮live2d_settings['waifuSize'] = '300x267.5'; // 看板娘大小live2d_settings['waifuTipsSize'] = '285x75'; // 提示框大小live2d_settings['waifuFontSize'] = '15px'; // 提示框字体live2d_settings['waifuToolFont'] = '18px'; // 工具栏字体live2d_settings['waifuToolLine'] = '25px'; // 工具栏行高live2d_settings['waifuToolTop'] = '-30px'; // 工具栏顶部边距live2d_settings['waifuDraggable'] = 'axis-x'; // 拖拽样式/* 在 initModel 前添加 */initModel('assets/waifu-tips.json?v=1.4.2'", 3000);
-// setTimeout('console.clear();', 100);
+setTimeout('console.clear();', 100);
